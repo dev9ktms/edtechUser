56844811b0559d6091f7068d1b94c960177762f2
imshresth0810 <gaurav.shresth2000@gmail.com>
2023-03-05 13:19:05 +0530

    user homepage

--- src/componets/home/index.js
+++ src/componets/home/index.js
@@ -1,38 +1,58 @@
-import React from "react";
-import Record_home from "./record_home.json";
+import React, { useState, useEffect} from "react";
+// import Record_home from "./record_home.json";
 import "./home.css";
 import Nav from "../nav";
 
 
 
 
-function index() {
+
+
+function Index() {
+
+  const [datav, setDatav] = useState([]);
+
+  const getVideo = async () => {
+    const response = await fetch(
+      `https://ed-tech-service-backend.onrender.com/homevideo/gethomevideo`,
+      {
+        method: "GET",
+      }
+    );
+    const json = await response.json();
+    setDatav(json);
+  };
+
+  useEffect(() => {
+    getVideo();
+  }, []);
+
+  console.log("==> ", datav)
+
+  const Record_home = datav;
   return (
     <>
     <Nav/>
-    <div>
-     <div className="home">
+      <div className="home">
         {Record_home &&
           Record_home.map((record) => {
             return (
               <span className="home-body" key={record.id}>
-                {/* <img src={record.source} alt=""  className="video"/> */}
                 <video
-                        width="100%"
-                        height="100%"
-                        src={record.source}
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        controls
-                        controlsList="nodownload"
-                      ></video>
+                  width="100%"
+                  height="100%"
+                  src={record.videoLink}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  controls
+                  controlsList="nodownload"
+                ></video>
               </span>
             );
           })}
-      </div>
       </div>
     </>
   );
 }
 
-export default index;
+export default Index;
